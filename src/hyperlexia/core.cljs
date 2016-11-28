ecad9666fae54134c701df1a0bfe3f12268d11f0
mathpunk <tom@mathpunk.net>
2016-11-28 14:34:56 -0800

    Fiddlin' with spec

--- src/hyperlexia/core.cljs
+++ src/hyperlexia/core.cljs
@@ -8,6 +8,34 @@
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest]]))
 
+;; Things that may be identified.
+;; Option 1: Require an id key.
+;; (s/def ::item (s/keys :req [::id]))
+;; Option 2: Require that =id= maybe be called on it.
+;; (s/def ::item #(id %))
+
+;; Things resembling bookmarks.
+(s/def ::href (s/and string? #(re-matches #"https?://.*" %)))
+(s/def ::pin (s/keys :req [::href]))
+
+(defc spec-href [href]
+  [:p (str (s/valid? ::href href) )])
+
+(defcard specify-href-false
+  (spec-href "hi"))
+
+(defcard specify-href-true
+  (spec-href "https://twitter.com/"))
+
+(s/def ::tags (s/or :string string? :set set?))
+(s/def ::tagged (s/and (s/keys :req [::tags])
+                       #(apply (complement empty?) [(:tags %)])))
+
+;; Things true about tweets.
+(def tweet-regex #"https?://twitter.com/(\w+)/status/(\d+)")
+(s/def ::tweet-url (s/and string? #(re-matches tweet-regex %)))
+(s/def ::tweet (s/and ::pin #(s/valid? ::tweet-url (:href %))))
+
 (enable-console-print!)
 
 ;; Items
@@ -22,6 +50,14 @@
 ;; Tweets are items whose :href conforms to ::tweet-url.
 
 ;; This =conform= idea was a sort of fakey typing but I think you should re-write this to use your specs.
+
+(def example-tweet-pin
+  {:id 803256287622549504
+   :href "https://twitter.com/zeynep/status/803256287622549504"})
+
+(defcard specification
+  (defc specification-of-pin []
+    [:p (str (s/valid? ::pin example-tweet-pin) )]))
 
 (defn conform [pin]
   (let [href (:href pin)
