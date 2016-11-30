268b8301f228416e6463c14641472019c1b5c7a9
mathpunk <tom@mathpunk.net>
2016-11-29 19:53:37 -0800

    Just a regular :href key will be fine

--- src/hyperlexia/item.cljs
+++ src/hyperlexia/item.cljs
@@ -1,7 +1,7 @@
 (ns hyperlexia.item
   (:require [cljs.spec :as s]
             [cljs.test :refer [testing is]])
-  (:require-macros [devcards.core :as dc :refer [deftest]]))
+  (:require-macros [devcards.core :as dc :refer [defcard deftest]]))
 
 ;; Identifiable
 (defprotocol Identifiable (id [x] "Return a context-appropriate unique identifier."))
@@ -9,7 +9,7 @@
 (s/def ::item (s/or :has-key (s/keys :req-un [::id]) :has-fn #(satisfies? Identifiable %)))
 
 (s/def ::href (s/and string? #(re-matches #"https?://.*" %)))
-(s/def ::pin (s/keys :req [::href]))
+(s/def ::pin (s/keys :req-un [::href]))
 
 (deftest href-specification
   (testing "string URLs"
@@ -18,38 +18,7 @@
     (is (not (s/valid? ::href "hi there")))))
 
 (deftest pin-specification
-  (testing "simple object with href key"
-    (is (s/valid? ::pin {:href "https://twitter.com"
-                         :id "23412343"}))
-    (is (not (s/valid? ::pin {:href "hey cool"
-                              :id 4434224})))))
-
-(def tweet-regex #"https?://twitter.com/(\w+)/status/(\d+)")
-(s/def ::tweet-url (s/and string? #(re-matches tweet-regex %)))
-
-(s/def ::tweet (s/and ::pin #(s/valid? ::tweet-url (:href %))))
-
-(deftest twitter-href-specification
-  (testing "twitter URLs"
-    (is (s/valid? ::tweet-url "https://twitter.com/zeynep/status/803256287622549504"))
-    (is (not (s/valid? ::tweet-url "hi there")))
-    (is (not (s/valid? ::tweet-url "https://thenewyorktimes.com")))))
-
-#_(deftest tweet-destructuring
-    (testing "url -> tweet with data"
-      (let [href "https://twitter.com/mathpunk/status/123412341234"
-            tweet (make-tweet href)]
-        (is (= "zeynep" (:user tweet)))
-        (is (= "803256287622549504" (:id tweet))))))
-
-(deftest testing-works
-  (is (= 1 1)))
-
-(deftest fail-testing-works
-  (is (= 2 1)))
-
-(deftest fail-testing-still-works
-  (is (= 3 1)))
-
-(deftest success-testing-still-works
-  (is (= 2 (+ 1 1))))
+  (testing "simple pin object"
+    (is (s/valid? ::pin {:href "https://twittre.com"}))
+    (is (not (s/valid? ::pin {})))
+    (is (not (s/valid? ::pin {:href "clik me"})))))
