07c0c8fce56a1aad42267a3267bbfcf2e4fb3904
mathpunk <tom@mathpunk.net>
2016-11-29 20:14:42 -0800

    Section the literate code further; add next links

--- src/hyperlexia/item.cljs
+++ src/hyperlexia/item.cljs
@@ -22,3 +22,38 @@
     (is (s/valid? ::pin {:href "https://twittre.com"}))
     (is (not (s/valid? ::pin {})))
     (is (not (s/valid? ::pin {:href "clik me"})))))
+
+(def tweet-regex #"https?://twitter.com/(\w+)/status/(\d+)")
+(s/def ::tweet-url (s/and string? #(re-matches tweet-regex %)))
+
+(s/def ::tweet (s/and ::pin #(s/valid? ::tweet-url (:href %))))
+
+(deftest twitter-href-specification
+  (testing "twitter URLs"
+    (is (s/valid? ::tweet-url "https://twitter.com/mathpunk/status/1234123412341234"))
+    (is (not (s/valid? ::tweet-url "hi there")))
+    (is (not (s/valid? ::tweet-url "https://thenewyorktimes.com")))))
+
+(deftest twitter-pin-specification
+  (testing "simple objects with and without twitter URLs"
+    (is (s/valid? ::tweet {:href "https://twitter.com/mathpunk/status/1234123412341234"}))
+    (is (not (s/valid? ::tweet {:freetext "some words"})))
+    (is (not (s/valid? ::tweet {:href "https://gogole.com"})))
+    ))
+
+(defn href->tweet [href]
+  (let [regex #"https://twitter.com/(\w+)/status/(\d+).*"]
+    (if-let [matches (first (re-seq regex href))]
+      (let [id (nth matches 2)
+            user (nth matches 1)]
+        {:href href
+         :user user
+         :id id})
+      {:href href})))
+
+  (deftest tweet-destructuring
+    (testing "url -> tweet with data"
+      (let [href "https://twitter.com/mathpunk/status/1234123412341234"
+            tweet (href->tweet href)]
+        (is (= "mathpunk" (:user tweet)))
+        (is (= "1234123412341234" (:id tweet))))))
